49d4c51d514bfeef4e00ac5290dda92f88e39457
CicalAkul <lukalacic05@gmail.com>
2023-12-01 11:47:09 +0100

    add deposita and preleva methods

--- src/Cassaforte.java
+++ src/Cassaforte.java
@@ -6,11 +6,13 @@ public class Cassaforte {
     private boolean stato; // true: aperto, false: chiuso
     private boolean allarme; // true: attivo, false: disattivato
     private int ultimoOperatore; //ultimo operatore che ha avuto accesso alla cassaforte
+    private int soldi;
     GestorePersonale operatoriAbilitati = new GestorePersonale(); //lista del personale che ha accesso alla cassaforte
 
     public Cassaforte() { // costruttore dell cassaforte
         this.stato = false;
         this.allarme = false;
+        this.soldi = 1000000;
     }
 
     public void apri(int numeroMatricola) {
@@ -45,6 +47,36 @@ public class Cassaforte {
     public void disattivaAllarme() {
         allarme = false;
         System.out.println("Allarme disattivato.");
+    }
+    public void preleva(int soldiDaPrelevare) {
+        if (stato) {
+            if (allarme) {
+                System.out.println("Non è possibile prelevare soldi con l'allarme attivo.");
+            } else {
+                if (soldiDaPrelevare < soldi) {
+                    soldi -= soldiDaPrelevare;
+                } else {
+                    soldiDaPrelevare = soldi;
+                    soldi = 0;
+                }
+                System.out.println("Hai prelevato " + soldiDaPrelevare + " euro.");
+            }
+        } else {
+            System.out.println("Non si possono prelevare soldi. La cassaforte è chiusa.");
+        }
+    }
+    // Metodo per depositare soldi nella cassaforte
+    public void deposita(int soldiDaDepositare) {
+        if (stato) {
+            if (allarme) {
+                System.out.println("Non è possibile depositare soldi con l'allarme attivo.");
+            } else {
+                soldi += soldiDaDepositare;
+                System.out.println("Hai depositato " + soldiDaDepositare + " euro.");
+            }
+        } else {
+            System.out.println("Non si possono depositare soldi. La cassaforte è chiusa.");
+        }
     }
 }
 
