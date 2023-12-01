d9eba0570d224dc07c346cf41221c6332db4d20f
albertofantinato_ <111857261+Albo05@users.noreply.github.com>
2023-12-01 11:36:31 +0100

    Modifiche apportate: Cassaforte:  - sistemato costruttore

--- src/Cassaforte.java
+++ src/Cassaforte.java
@@ -1,31 +1,39 @@
 // Classe Cassaforte
+
+
+
 public class Cassaforte {
-    private String combinazione;
     private boolean stato; // true: aperto, false: chiuso
     private boolean allarme; // true: attivo, false: disattivato
+    private int ultimoOperatore; //ultimo operatore che ha avuto accesso alla cassaforte
+    GestorePersonale operatoriAbilitati = new GestorePersonale(); //lista del personale che ha accesso alla cassaforte
 
-    public Cassaforte(String combinazione) {
-        this.combinazione = combinazione;
+    public Cassaforte() { // costruttore dell cassaforte
         this.stato = false;
         this.allarme = false;
     }
 
-    public void apri(String combinazioneInserita) {
-        if (combinazioneInserita.equals(combinazione)) {
-            stato = true;
-            System.out.println("Cassaforte aperta.");
+    public void apri(int numeroMatricola) {
+        ultimoOperatore = numeroMatricola; // memorizzo l'ultimo operatore che ha avuto accesso alla cassaforte
+        if (operatoriAbilitati.cercaImpiegato(numeroMatricola) != null) { //controllo se la combianzione inserita è corretta
+            stato = true; // lo stato della cassaforte diventa aperto
+            allarme = false; // viene disattivato l'allarme
+
+            System.out.println("Cassaforte aperta correttamente.");
         } else {
+            allarme = true;
             System.out.println("Combinazione errata. Cassaforte non aperta.");
         }
     }
 
     public void chiudi() {
         stato = false;
+        allarme = false;
         System.out.println("Cassaforte chiusa.");
     }
 
     public void cambiaCombinazione(String nuovaCombinazione) {
-        combinazione = nuovaCombinazione;
+        //combinazione = nuovaCombinazione;
         System.out.println("Combinazione cambiata.");
     }
 
