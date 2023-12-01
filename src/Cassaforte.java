d09f201e035d0f4fc2b2c681405403326991b69f
CicalAkul <lukalacic05@gmail.com>
2023-12-01 11:29:58 +0100

    add preleva and deposita methods

--- src/Cassaforte.java
+++ src/Cassaforte.java
@@ -3,11 +3,13 @@ public class Cassaforte {
     private String combinazione;
     private boolean stato; // true: aperto, false: chiuso
     private boolean allarme; // true: attivo, false: disattivato
+    private int soldi;
 
     public Cassaforte(String combinazione) {
         this.combinazione = combinazione;
         this.stato = false;
         this.allarme = false;
+        this.soldi = 1000000;
     }
 
     public void apri(String combinazioneInserita) {
@@ -37,6 +39,38 @@ public class Cassaforte {
     public void disattivaAllarme() {
         allarme = false;
         System.out.println("Allarme disattivato.");
+    }
+
+    // Metodo per prelevare soldi dalla cassaforte
+    public void preleva(int soldiDaPrelevare) {
+        if (stato) {
+            if (allarme) {
+                System.out.println("Non è possibile prelevare soldi con l'allarme attivo.");
+            } else {
+                if (soldiDaPrelevare < soldi) {
+                    soldi -= soldiDaPrelevare;
+                } else {
+                    soldiDaPrelevare = soldi;
+                    soldi = 0;
+                }
+                System.out.println("Hai prelevato " + soldiDaPrelevare + " euro.");
+            }
+        } else {
+            System.out.println("Non si possono prelevare soldi. La cassaforte è chiusa.");
+        }
+    }
+    // Metodo per depositare soldi nella cassaforte
+    public void deposita(int soldiDaDepositare) {
+        if (stato) {
+            if (allarme) {
+                System.out.println("Non è possibile depositare soldi con l'allarme attivo.");
+            } else {
+                soldi += soldiDaDepositare;
+                System.out.println("Hai depositato " + soldiDaDepositare + " euro.");
+            }
+        } else {
+            System.out.println("Non si possono depositare soldi. La cassaforte è chiusa.");
+        }
     }
 }
 
